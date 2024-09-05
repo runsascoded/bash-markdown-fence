c24339a1323b4e0fd793f93da48dc8034a0d5354
Ryan Williams <ryan.williams@tiledb.com>
2024-09-04 20:10:56 -0400

    v0.0.3: add `url` to `setup`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from utz.setup import setup
 
 setup(
     name="bmdf",
-    version="0.0.2",
+    version="0.0.3",
     entry_points={
         "console_scripts": [
             "bmd = bmdf.cli:bmd",
@@ -11,5 +11,6 @@ setup(
             "bmdfff = bmdf.cli:bmd_fff",
             "mdcmd = mdcmd.cli:main",
         ]
-    }
+    },
+    url="https://github.com/runsascoded/bash-markdown-fence",
 )
